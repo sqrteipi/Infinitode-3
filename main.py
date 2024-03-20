03562b702c06b931813ef58ccec97448b811c0bc
sqrteipi <wongheukiu@gmail.com>
2024-03-20 17:58:33 +0800

    Basic Structure of main.py

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("Hello World")
+import pygame
+
+pygame.init()
+
+# FULL SCREEN
+SCREEN_WIDTH = 1920
+SCREEN_HEIGHT = 1080
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+clock = pygame.time.Clock()
+running = True
+dt = 0
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    pygame.display.flip()
+    dt = clock.tick(60) / 1000
+
+pygame.quit()
