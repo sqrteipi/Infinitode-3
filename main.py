920ac12b1d1584c9dcde23def4f3b05dff789638
sqrteipi <wongheukiu@gmail.com>
2024-03-20 21:11:53 +0800

    Added PixelArray for testing

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ pygame.init()
 # FULL SCREEN
 SCREEN_WIDTH = 1920
 SCREEN_HEIGHT = 1080
+GRID_SIZE = 25
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+surface = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 clock = pygame.time.Clock()
 running = True
@@ -15,7 +17,21 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    
+
+    pixels = pygame.PixelArray(surface)
+
+    for x in range(0, SCREEN_WIDTH, GRID_SIZE):
+        for y in range(0, SCREEN_HEIGHT, GRID_SIZE):
+            if ((x + y) & 1) == 0:
+                pixels[x:x+GRID_SIZE, y:y+GRID_SIZE] = (64, 64, 64)
+            else:
+                pixels[x:x+GRID_SIZE, y:y+GRID_SIZE] = (192, 192, 192)
+
+    pixels.close()
+
+    display = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    display.blit(surface, (0, 0))
+
     pygame.display.flip()
     dt = clock.tick(60) / 1000
 
